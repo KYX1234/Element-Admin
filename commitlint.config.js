c558c40474dcc289479e042faf7735c2a0a460e3
17702858513 <1059784729@qq.com>
2023-07-19 16:38:30 +0800

    chore: commit注释

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-leading-blank': [2, 'always'],
@@ -7,25 +6,23 @@ module.exports = {
     'header-max-length': [2, 'always', 108],
     'subject-empty': [2, 'never'],
     'type-empty': [2, 'never'],
+    'subject-case': [0],
     'type-enum': [
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'perf',
-        'style',
-        'docs',
-        'test',
-        'refactor',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-        'wip',
-        'workflow',
-        'types',
-        'release'
+        'feat', // 新功能
+        'fix', // 修复bug
+        'perf', // 性能优化
+        'style', // 修改代码格式，不影响代码逻辑
+        'docs', // 修改文档
+        'test', // 修改测试用例
+        'refactor', // 代码重构
+        'build', // 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）
+        'ci', // 持续集成
+        'chore', // 其它提交
+        'revert', // 代码回滚
+        'wip' // 功能开发中
       ]
     ]
   }
