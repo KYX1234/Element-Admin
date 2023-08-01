a87a85e404c4555f28999ff4263e0eec9f83500e
17702858513 <1059784729@qq.com>
2023-08-01 17:54:31 +0800

    build: 降低 vite-plugin-mock版本修复异常问题

--- build/plugins/mock.ts
+++ build/plugins/mock.ts
@@ -1,11 +1,10 @@
 import { viteMockServe } from 'vite-plugin-mock'
 
 export default (viteEnv: ImportMetaEnv) => {
-  const prodMock = viteEnv.VITE_PROD_MOCK === 'Y' || true
+  const prodMock = viteEnv.VITE_PROD_MOCK === 'Y'
   return viteMockServe({
     mockPath: 'mock',
-    enable: prodMock, //是否启用mock开关(默认开启)
-    watchFiles: false,
+    prodEnabled: prodMock, //生产是否启用mock开关(默认开启)
     configPath: `
 			import { setupProdMockServer } from '../../mock';
 			setupProdMockServer()
