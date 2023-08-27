23f7e48c07cd6083e1342614122cb75134675fa2
KYX1234 <KYX1234@users.noreply.github.com>
2023-08-27 14:03:35 +0000

    deploy: 65b990e303f51c71fe054fd722af5259d39463c5

---
+++ static/js/404-f3d2231d.js
@@ -0,0 +1,1 @@
+import{j as o,w as t,o as r,m as a}from"./index-4dd64c7d.js";import{E as c}from"./el-card-01bfe788.js";import{_ as s}from"./_plugin-vue_export-helper-c27b6911.js";const _={},n=a("div",null,"404-待开发",-1);function d(f,m){const e=c;return r(),o(e,{shadow:"never"},{default:t(()=>[n]),_:1})}const u=s(_,[["render",d]]);export{u as default};
