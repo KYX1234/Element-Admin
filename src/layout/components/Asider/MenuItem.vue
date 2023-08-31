b18d71f3a0c5b90cbf6bcebe2649fe5313d8fcdf
17702858513 <1059784729@qq.com>
2023-08-31 12:29:31 +0800

    feat: 菜单文字溢出

--- src/layout/components/Asider/MenuItem.vue
+++ src/layout/components/Asider/MenuItem.vue
@@ -2,13 +2,13 @@
   <el-sub-menu :index="menu.path" v-if="menu.children && menu.children.length > 0">
     <template #title>
       <Icon :name="menu.icon" size="18" v-if="menu.icon" />
-      <span>{{ menu.name }}</span>
+      <span class="truncate">{{ menu.name }}</span>
     </template>
     <MenuItem v-for="menuItem in menu.children" :key="menuItem.path" :menu="menuItem" />
   </el-sub-menu>
   <el-menu-item :index="menu.path" v-else>
     <Icon :name="menu.icon" size="18" v-if="menu.icon" />
-    <span>{{ menu.name }}</span>
+    <span class="truncate">{{ menu.name }}</span>
   </el-menu-item>
 </template>
 
