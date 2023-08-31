5542f89a22eb1bc0bb816a41409d0527929585b2
17702858513 <1059784729@qq.com>
2023-08-31 16:11:16 +0800

    chore: 测试

--- src/views/dashboard/workbench/index.vue
+++ src/views/dashboard/workbench/index.vue
@@ -1,28 +1,36 @@
 <template>
   <el-card shadow="never">
-    <div class="flex justify-between">
-      <div class="flex-y-center">
-        <img
-          class="w-20 mr-5 rounded-full border border-borderColor"
-          src="@/assets/image/avatar.jpg"
-          alt=""
-        />
-        <div>
-          <h3 class="font-bold">早上好，又是美好的一天呢!</h3>
-          <p class="text-sm text-gray leading-9">今天天气晴朗，20℃-30℃</p>
+    <el-row justify="space-between">
+      <el-col :span="10" :xs="24">
+        <div class="flex h-full py-2">
+          <img
+            class="w-20 h-20 mr-5 rounded-full border border-borderColor"
+            src="@/assets/image/avatar.jpg"
+            alt=""
+          />
+          <div>
+            <h3 class="mb-2">早上好，又是美好的一天呢!</h3>
+            <p class="text-sm text-gray">今天天气晴朗，20℃-30℃</p>
+          </div>
         </div>
-      </div>
-      <div class="flex items-center">
-        <div class="mr-7">
-          <div class="text-gray">消息</div>
-          <div class="text-6">22</div>
+      </el-col>
+      <el-col :span="8" :xs="24">
+        <div class="flex justify-around py-2">
+          <div class="mr-7">
+            <div class="text-gray mb-2">消息</div>
+            <div class="text-6">22</div>
+          </div>
+          <div>
+            <div class="text-gray mb-2">代办</div>
+            <div class="text-6">4/10</div>
+          </div>
+          <div>
+            <div class="text-gray mb-2">项目</div>
+            <div class="text-6">10</div>
+          </div>
         </div>
-        <div>
-          <div class="text-gray">代办</div>
-          <div class="text-6">4/10</div>
-        </div>
-      </div>
-    </div>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
