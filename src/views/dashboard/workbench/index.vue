dd1934854a2ad0122490577fd53974364be32e4d
17702858513 <1059784729@qq.com>
2023-08-31 15:34:08 +0800

    style: 工作台样式

--- src/views/dashboard/workbench/index.vue
+++ src/views/dashboard/workbench/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <el-card shadow="never">工作台-待开发</el-card>
+  <el-card shadow="never">
+    <div class="flex justify-between">
+      <div class="flex-y-center">
+        <img
+          class="w-20 mr-5 rounded-full border border-borderColor"
+          src="@/assets/image/avatar.jpg"
+          alt=""
+        />
+        <div>
+          <h3 class="font-bold">早上好，又是美好的一天呢!</h3>
+          <p class="text-sm text-gray leading-9">今天天气晴朗，20℃-30℃</p>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <div class="mr-7">
+          <div class="text-gray">消息</div>
+          <div class="text-6">22</div>
+        </div>
+        <div>
+          <div class="text-gray">代办</div>
+          <div class="text-6">4/10</div>
+        </div>
+      </div>
+    </div>
+  </el-card>
 </template>
 
 <script lang="ts" setup></script>
