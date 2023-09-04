4e70076e5f00e9b0ea8e1aa83c16ada8016631ae
KYX1234 <KYX1234@users.noreply.github.com>
2023-09-04 09:25:06 +0000

    deploy: 0ffe50c8f981c8a6757409d1dc920aa4816da5a7

---
+++ static/js/index-cdea91c1.js
@@ -0,0 +1,1 @@
+import{h as o,w as r,o as t,x as a}from"./index-26b4a99b.js";import{E as c}from"./el-card-48f1dae6.js";import{_ as n}from"./_plugin-vue_export-helper-c27b6911.js";const s={};function _(d,f){const e=c;return t(),o(e,{shadow:"never"},{default:r(()=>[a("二级菜单")]),_:1})}const x=n(s,[["render",_]]);export{x as default};
