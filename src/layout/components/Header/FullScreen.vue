cd6d170124782f00eac7ecb0bfc842247eae4e3b
17702858513 <1059784729@qq.com>
2023-09-07 12:15:33 +0800

    fix: 修复全屏图标显示

--- src/layout/components/Header/FullScreen.vue
+++ src/layout/components/Header/FullScreen.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-center h-14 w-10 cursor-pointer hover:bg-fill" @click="toggle">
-    <Icon name="local-icon-fullscreen" size="20" v-if="isFullscreen" />
+    <Icon name="local-icon-fullscreen" size="20" v-if="!isFullscreen" />
     <Icon name="local-icon-fullscreen-exit" size="20" v-else />
   </div>
 </template>
