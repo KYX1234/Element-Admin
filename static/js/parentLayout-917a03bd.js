3278690336c02e96ff452982709a9919e4be4154
KYX1234 <KYX1234@users.noreply.github.com>
2023-09-22 02:21:52 +0000

    deploy: 386752a70e26952730e31e3d5537ec536994a7d7

---
+++ static/js/parentLayout-917a03bd.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{aj as r,e as t,o as n}from"./index-0918650a.js";const c={};function a(s,_){const e=r("router-view");return n(),t(e)}const m=o(c,[["render",a]]);export{m as default};
